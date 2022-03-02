56e38e6efe2061d3cd3626ea8501c565f47062c9
hannahhan153 <hannah.han@biola.edu>
2022-03-01 21:22:17 -0500

    Create Front-End Functionality for Zookeepers

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,4 +1,6 @@
+const $zookeeperForm = document.querySelector("#zookeeper-form");
 const $displayArea = document.querySelector('#display-area');
+
 
 const printResults = resultArr => {
   console.log(resultArr);
@@ -21,10 +23,16 @@ const printResults = resultArr => {
 };
 
 const getZookeepers = () => {
-  fetch('/api/zookeepers')
+  let queryUrl = '/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl += `${key}=${value}&`;
+  });
+
+  fetch(queryUrl)
     .then(response => {
       if (!response.ok) {
-        return alert('Error: ' + response.statusText);
+        return alert(`Error: ${response.statusText}`);
       }
       return response.json();
     })
@@ -34,4 +42,18 @@ const getZookeepers = () => {
     });
 };
 
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObject = { name, age };
+
+  getZookeepers(zookeeperObject);
+};
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 getZookeepers();
